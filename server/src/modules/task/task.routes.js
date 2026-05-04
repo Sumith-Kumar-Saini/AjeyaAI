1511b276861b328f10c87f93b499a1eef558d6ae
henil88 <henilrathod88@gmail.com>
2026-05-04 13:29:07 +0530

    feat: implement validation and error handling in task creation and retrieval

--- server/src/modules/task/task.routes.js
+++ server/src/modules/task/task.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { createTasks } from "./task.controller.js";
+import { createTasks, getTasksByFeature } from "./task.controller.js";
+import {} from "./task.service.js";
 
-export const taskRoutes =
-  Router();
+export const taskRoutes = Router();
 
-taskRoutes.post(
-  "/:id/generate",
-  createTasks
+taskRoutes.post("/:id/generate", createTasks);
+taskRoutes.get(
+  "/:resultId/features/:featureId",
+  getTasksByFeature
 );
